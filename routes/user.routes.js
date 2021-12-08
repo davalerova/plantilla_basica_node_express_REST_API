d98ed15dc586d1d38b070521af60881fb8e3b9e9
David Valero <dvdovni@gmail.com>
2021-12-08 16:05:11 -0500

    Se valida rol con la base de datos

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,10 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
+
+const Role = require('../models/role');
+
+const { validarCampos } = require('../middlewares/validar-campos');
+
 const {
   usersGet,
   usersPut,
@@ -7,7 +12,6 @@ const {
   userDelete,
   userPatch,
 } = require('../controllers/user.controllers');
-const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
@@ -17,16 +21,22 @@ router.put('/:id', usersPut);
 
 router.post(
   '/',
-  [check('name', 'El nombre es obligatorio').not().isEmpty()],
   [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
     check(
       'password',
       'La contraseña es obligatoria y debe contener más de 6 caracteres'
     ).isLength({ min: 6 }),
-  ],
-  [check('email', 'El correo no es válido').isEmail()],
-  [
-    check('role', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('email', 'El correo no es válido').isEmail(),
+    // [
+    //   check('role', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    // ],
+    check('role').custom(async (role = '') => {
+      const roleExist = await Role.findOne({ role });
+      if (!roleExist) {
+        throw new Error(`El rol ${role} no está registrado en la BD`);
+      }
+    }),
     validarCampos,
   ],
   userPost
